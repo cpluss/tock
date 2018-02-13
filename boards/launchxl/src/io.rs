ed02eca76674451e1b638682cb10f1cdf62fecf0
sebastian <me@sebastianlund.se>
2018-02-13 10:03:09 +0100

    launchxl: connect uart with panic handler
    Add a simple panic handler and connect the uart driver.

--- boards/launchxl/src/io.rs
+++ boards/launchxl/src/io.rs
@@ -1,26 +1,65 @@
-use cc26xx;
-use core::fmt::{Arguments, Write};
+use core::fmt::{write, Arguments, Write};
 use kernel::debug;
 use kernel::hil::led;
+use kernel::hil::uart::{self, UART};
+use cc26x2;
+use cc26xx;
 
-struct Writer {}
+pub struct Writer {
+    initialized: bool,
+}
 
-static mut WRITER: Writer = Writer {};
+pub static mut WRITER: Writer = Writer { initialized: false };
 
 impl Write for Writer {
-    fn write_str(&mut self, _s: &str) -> ::core::fmt::Result {
+    fn write_str(&mut self, s: &str) -> ::core::fmt::Result {
+        let uart = unsafe { &mut cc26x2::uart::UART0 };
+        if !self.initialized {
+            self.initialized = true;
+            uart.init(uart::UARTParams {
+                baud_rate: 115200,
+                stop_bits: uart::StopBits::One,
+                parity: uart::Parity::None,
+                hw_flow_control: false,
+            });
+        }
+        for c in s.bytes() {
+            uart.send_byte(c);
+            while !uart.tx_ready() {}
+        }
         Ok(())
     }
+}
+
+#[macro_export]
+macro_rules! print {
+        ($($arg:tt)*) => (
+            {
+                use core::fmt::write;
+                let writer = &mut $crate::io::WRITER;
+                let _ = write(writer, format_args!($($arg)*));
+            }
+        );
+}
+
+#[macro_export]
+macro_rules! println {
+        ($fmt:expr) => (print!(concat!($fmt, "\r\n")));
+            ($fmt:expr, $($arg:tt)*) => (print!(concat!($fmt, "\r\n"), $($arg)*));
 }
 
 #[cfg(not(test))]
 #[lang = "panic_fmt"]
 #[no_mangle]
-pub unsafe extern "C" fn rust_begin_unwind(args: Arguments, file: &'static str, line: u32) -> ! {
+pub unsafe extern "C" fn rust_begin_unwind(
+    _args: Arguments,
+    _file: &'static str,
+    _line: usize,
+) -> ! {
     // 6 = Red led, 7 = Green led
     const LED_PIN: usize = 6;
 
     let led = &mut led::LedLow::new(&mut cc26xx::gpio::PORT[LED_PIN]);
     let writer = &mut WRITER;
-    debug::panic(led, writer, args, file, line)
+    debug::panic(led, writer, args, file, line);
 }
