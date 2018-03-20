400a72f1fb76364add1b89c5ea332c5edab1d6ec
Filip <filip.nilsson@blixtmail.se>
2018-03-20 16:31:39 +0100

    launchxl: Converted all I2C functions to use the new reg-interface

--- chips/cc26xx/src/i2c.rs
+++ chips/cc26xx/src/i2c.rs
@@ -8,24 +8,17 @@ use kernel::hil::gpio::Pin;
 pub const I2C_MCR_MFE: u32 = 0x10;
 pub const I2C_MCTRL_RUN: u32 = 0x1;
 
-pub const I2C_MASTER_CMD_SINGLE_SEND: u32 = 0x7;
-pub const I2C_MASTER_CMD_BURST_SEND_ERROR_STOP: u32 = 0x4;
-pub const I2C_MASTER_CMD_BURST_RECEIVE_START: u32 = 0xb;
-pub const I2C_MASTER_CMD_BURST_RECEIVE_CONT: u32 = 0x9;
-pub const I2C_MASTER_CMD_BURST_SEND_START: u32 = 0x3;
-pub const I2C_MASTER_CMD_BURST_SEND_CONT: u32 = 0x1;
-pub const I2C_MASTER_CMD_BURST_SEND_FINISH: u32 = 0x5;
-pub const I2C_MASTER_CMD_BURST_RECEIVE_FINISH: u32 = 0x5;
+// I2C commands
+const SINGLE_SEND: u32 = 0x7;
+const BURST_SEND_ERROR_STOP: u32 = 0x4;
+const BURST_RECEIVE_START: u32 = 0xb;
+const BURST_RECEIVE_CONT: u32 = 0x9;
+const BURST_SEND_START: u32 = 0x3;
+const BURST_SEND_CONT: u32 = 0x1;
+const BURST_SEND_FINISH: u32 = 0x5;
+const BURST_RECEIVE_FINISH: u32 = 0x5;
 
-pub const I2C_MSTAT_ERR: u32 = 0x2;
-pub const I2C_MSTAT_BUSY: u32 = 0x1;
-pub const I2C_MSTAT_BUSBSY: u32 = 0x40;
-pub const I2C_MSTAT_ARBLST: u32 = 0x10;
-pub const I2C_MSTAT_DATACK_N: u32 = 0x8;
-pub const I2C_MSTAT_ADRACK_N: u32 = 0x4;
-pub const I2C_MSTAT_DATACK_N_M: u32 = 0x8;
-pub const I2C_MSTAT_ADRACK_N_M: u32 = 0x4;
-
+// Pin configuration
 pub const BOARD_IO_SDA: usize = 0x5;
 pub const BOARD_IO_SCL: usize = 0x6;
 pub const BOARD_IO_SDA_HP: usize = 0x8;
@@ -53,15 +46,36 @@ pub struct Registers {
     _reserved0: [u8; 0x7e4],
 
     pub msa: ReadWrite<u32>,
-    pub mstat_mctrl: ReadWrite<u32>,
+    pub mstat_mctrl: ReadWrite<u32, MasterStatusAndControl::Register>,
     pub mdr: ReadWrite<u32>,
-    pub mtpr: ReadWrite<u32>,
+    pub mtpr: ReadWrite<u32, TimerPeriod::Register>,
     pub mimr: ReadWrite<u32>,
     pub mris: ReadOnly<u32>,
     pub mmis: ReadOnly<u32>,
     pub micr: WriteOnly<u32>,
-    pub mcr: ReadWrite<u32>,
+    pub mcr: ReadWrite<u32, MasterConfiguration::Register>,
 }
+
+register_bitfields![
+    u32,
+    TimerPeriod [
+        SCL_CLOCK_PERIOD OFFSET(0) NUMBITS(7) []
+    ],
+    MasterConfiguration [
+        MASTER_FUNCTION_ENABLE OFFSET(4) NUMBITS(1) []
+    ],
+    MasterStatusAndControl [
+        // Control
+        ENABLE_MASTER OFFSET(0) NUMBITS(1) [],
+        // Status
+        CONTROLLER_BUSY OFFSET(0) NUMBITS(1) [],
+        ERROR OFFSET(1) NUMBITS(1) [],
+        BUS_BUSY OFFSET(6) NUMBITS(1) [],
+        ARBITRATION_LOST OFFSET(4) NUMBITS(1) [],
+        DATA_NOT_ACKED OFFSET(3) NUMBITS(1) [],
+        ADDRESS_NOT_ACKED OFFSET(2) NUMBITS(1) []
+    ]
+];
 
 pub const I2C_BASE: *mut Registers = 0x4000_2000 as *mut Registers;
 
@@ -108,21 +122,21 @@ impl I2C {
         // Compute SCL (serial clock) period
         let tpr = ((MCU_CLOCK + (2 * 10 * freq) - 1) / (2 * 10 * freq)) - 1;
         let regs: &Registers = unsafe { &*self.regs };
-        regs.mtpr.set(tpr);
+        regs.mtpr.write(TimerPeriod::SCL_CLOCK_PERIOD.val(tpr));
     }
 
     fn master_enable(&self) {
         let regs: &Registers = unsafe { &*self.regs };
         // Set as master
-        regs.mcr.set(regs.mcr.get() | I2C_MCR_MFE);
+        regs.mcr.modify(MasterConfiguration::MASTER_FUNCTION_ENABLE::SET);
         // Enable master to transfer/receive data
-        regs.mstat_mctrl.set(I2C_MCTRL_RUN);
+        regs.mstat_mctrl.write(MasterStatusAndControl::ENABLE_MASTER::SET);
     }
 
     fn master_disable(&self) {
         let regs: &Registers = unsafe { &*self.regs };
         regs.mstat_mctrl.set(0);
-        regs.mcr.set(regs.mcr.get() & !I2C_MCR_MFE);
+        regs.mcr.modify(MasterConfiguration::MASTER_FUNCTION_ENABLE::CLEAR);
     }
 
     pub fn write_single(&self, data: u8) -> bool {
@@ -133,7 +147,7 @@ impl I2C {
             return false;
         }
 
-        self.master_control(I2C_MASTER_CMD_SINGLE_SEND);
+        self.master_control(SINGLE_SEND);
         if !self.busy_wait_master() {
             return false;
         }
@@ -146,7 +160,7 @@ impl I2C {
 
         self.busy_wait_master_bus();
 
-        self.master_control(I2C_MASTER_CMD_BURST_RECEIVE_START);
+        self.master_control(BURST_RECEIVE_START);
 
         let mut i = 0;
         let mut success = true;
@@ -155,13 +169,13 @@ impl I2C {
             success = self.status();
             if success {
                 data[i as usize] = self.master_get_data() as u8;
-                self.master_control(I2C_MASTER_CMD_BURST_RECEIVE_CONT);
+                self.master_control(BURST_RECEIVE_CONT);
                 i += 1;
             }
         }
 
         if success {
-            self.master_control(I2C_MASTER_CMD_BURST_RECEIVE_FINISH);
+            self.master_control(BURST_RECEIVE_FINISH);
             self.busy_wait_master();
             success = self.status();
             if success {
@@ -180,7 +194,7 @@ impl I2C {
 
         self.busy_wait_master_bus();
 
-        self.master_control(I2C_MASTER_CMD_BURST_SEND_START);
+        self.master_control(BURST_SEND_START);
         self.busy_wait_master();
         let mut success = self.status();
 
@@ -190,14 +204,14 @@ impl I2C {
             }
             self.master_put_data(data[i as usize]);
             if i < len - 1 {
-                self.master_control(I2C_MASTER_CMD_BURST_SEND_CONT);
+                self.master_control(BURST_SEND_CONT);
                 self.busy_wait_master();
                 success = self.status();
             }
         }
 
         if success {
-            self.master_control(I2C_MASTER_CMD_BURST_SEND_FINISH);
+            self.master_control(BURST_SEND_FINISH);
             self.busy_wait_master();
             success = self.status();
             self.busy_wait_master_bus();
@@ -213,7 +227,7 @@ impl I2C {
 
         self.busy_wait_master_bus();
 
-        self.master_control(I2C_MASTER_CMD_BURST_SEND_START);
+        self.master_control(BURST_SEND_START);
         self.busy_wait_master();
         let mut success = self.status();
 
@@ -224,7 +238,7 @@ impl I2C {
 
             self.master_put_data(data[i as usize]);
 
-            self.master_control(I2C_MASTER_CMD_BURST_SEND_CONT);
+            self.master_control(BURST_SEND_CONT);
             self.busy_wait_master();
             success = self.status();
         }
@@ -235,7 +249,7 @@ impl I2C {
 
         self.set_master_slave_address(self.slave_addr.get(), true);
 
-        self.master_control(I2C_MASTER_CMD_BURST_RECEIVE_START);
+        self.master_control(BURST_RECEIVE_START);
 
         let mut i = 0;
         while i < (read_len - 1) && success {
@@ -243,13 +257,13 @@ impl I2C {
             success = self.status();
             if success {
                 data[i as usize] = self.master_get_data() as u8;
-                self.master_control(I2C_MASTER_CMD_BURST_RECEIVE_CONT);
+                self.master_control(BURST_RECEIVE_CONT);
                 i += 1;
             }
         }
 
         if success {
-            self.master_control(I2C_MASTER_CMD_BURST_RECEIVE_FINISH);
+            self.master_control(BURST_RECEIVE_FINISH);
             self.busy_wait_master();
             success = self.status();
             if success {
@@ -278,12 +292,12 @@ impl I2C {
 
     fn master_bus_busy(&self) -> bool {
         let regs: &Registers = unsafe { &*self.regs };
-        (regs.mstat_mctrl.get() & I2C_MSTAT_BUSBSY) != 0
+        regs.mstat_mctrl.is_set(MasterStatusAndControl::BUS_BUSY)
     }
 
     fn master_busy(&self) -> bool {
         let regs: &Registers = unsafe { &*self.regs };
-        (regs.mstat_mctrl.get() & I2C_MSTAT_BUSY) != 0
+        regs.mstat_mctrl.is_set(MasterStatusAndControl::CONTROLLER_BUSY)
     }
 
     // Limited busy wait for the master
@@ -314,30 +328,27 @@ impl I2C {
     }
 
     fn status(&self) -> bool {
-        let status = self.master_err();
-
-        if (status & (I2C_MSTAT_DATACK_N_M | I2C_MSTAT_ADRACK_N_M)) != 0 {
-            self.master_control(I2C_MASTER_CMD_BURST_SEND_ERROR_STOP);
-        }
-
-        status == 0
-    }
-
-    fn master_err(&self) -> u32 {
+        //let status = self.master_err();
         let regs: &Registers = unsafe { &*self.regs };
-        let err = regs.mstat_mctrl.get();
+        let mut success = true;
 
-        // If the master is busy there is not error to report
-        if (err & I2C_MSTAT_BUSY) == 1 {
-            return 0;
+        // If the master is busy there is no error to report
+        let busy = regs.mstat_mctrl.is_set(MasterStatusAndControl::CONTROLLER_BUSY);
+
+        if !busy {
+            if regs.mstat_mctrl.is_set(MasterStatusAndControl::ERROR) {
+                // See if message was not acknowledged
+                if regs.mstat_mctrl.is_set(MasterStatusAndControl::DATA_NOT_ACKED)
+                    || regs.mstat_mctrl.is_set(MasterStatusAndControl::ADDRESS_NOT_ACKED) {
+                    self.master_control(BURST_SEND_ERROR_STOP);
+                    success = false;
+                }
+            } else if regs.mstat_mctrl.is_set(MasterStatusAndControl::ARBITRATION_LOST) {
+                success = false;
+            }
         }
 
-        // Check for errors
-        if err & (I2C_MSTAT_ERR | I2C_MSTAT_ARBLST) != 0 {
-            return err & (I2C_MSTAT_ARBLST | I2C_MSTAT_DATACK_N | I2C_MSTAT_ADRACK_N);
-        } else {
-            return 0;
-        }
+        success
     }
 
     fn accessible(&self) -> bool {
